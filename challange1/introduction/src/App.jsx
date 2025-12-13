a42342b906ab6ac1ed4c5b38e30866b2f37cf98a
Rupok-Koiry <koiry.rupok@gmail.com>
2025-12-13 15:04:29 +0600

    1st challange comeplete

---
+++ challange1/introduction/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="box">
+        <img src="pp.png" alt="Profile" />
+        <div className="des">
+          <h1>RK</h1>
+          <p>
+            Hi, I'm Rupok Saha, a passionate web developer with expertise in
+            creating dynamic and user-friendly websites. I love coding and
+            bringing ideas to life through technology.
+          </p>
+          <span style={{ backgroundColor: "#FF6B6B" }}>Javascript</span>
+          <span style={{ backgroundColor: "#4ECDC4" }}>Html+css</span>
+          <span style={{ backgroundColor: "#FFE66D" }}>Web design </span>
+          <span style={{ backgroundColor: "#95E1D3" }}>Git and github </span>
+          <span style={{ backgroundColor: "#C7CEEA" }}>React</span>
+          <span style={{ backgroundColor: "#FF8B94" }}>python</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
