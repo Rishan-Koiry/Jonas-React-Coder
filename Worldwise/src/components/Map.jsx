75382dc0ccab8bbed87e0caef12265ab45115e63
Rupok-Koiry <koiry.rupok@gmail.com>
2026-01-11 19:17:10 +0600

    challange✅

--- Worldwise/src/components/Map.jsx
+++ Worldwise/src/components/Map.jsx
@@ -55,8 +55,8 @@ function Map() {
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png"
         />
         {cities.map((city) => (
           <Marker
